8d26e2cb73843ea0d108a9b71e5c03d428b32c2e
Daniel Esteban Chacon Diaz <138712462+DanielEStebanChaconDiaz@users.noreply.github.com>
2024-04-17 12:19:57 +0000

    feat: :rocket: Final pagina goku

--- js/main.js
+++ js/main.js
@@ -1,55 +1,134 @@
-import prompt from 'prompt-sync';
-let input = prompt();
-console.log("¿Que Akatsuki eres segun tu mes de nacimiento?");
-console.log('1. Enero. 2. Febrero. 3. Marzo. 4. Abril. 5. Mayo. 6. Junio. 7. Julio. 8. Agosto. 9. Septiembre. 10. Octubre. 11. Noviembre. 12. Diciembre. ');
-let n = input();
-while (n){
-    if (n == 1){
-        console.log("Itachi");
-        break;
-    }
-    else if (n == 2){
-        console.log("Konan");
-        break;
-    }
-    else if (n == 3){
-        console.log("Sasori");
-        break;
-    }
-    else if (n == 7){
-        console.log("Obito");
-        break;
-    }
-    else if (n == 4){
-        console.log("Pain");
-        break;
-    }
-    else if (n == 5){
-        console.log("Kisame");
-        break;
-    }
-    else if (n == 6){
-        console.log("Deidara");
-        break;
-    }
-    else if (n == 8){
-        console.log("Hidan");
-        break;
-    }
-    else if (n == 9){
-        console.log("Orochimaru");
-        break;
-    }
-    else if (n == 10){
-        console.log("Kakuzu");
-        break;
-    }
-    else if (n == 11){
-        console.log("Nagato");
-        break;
-    }
-    else if (n == 12){
-        console.log("Zetsu");
-        break;
-    }
-}
+var boton = document.getElementById("itachi");
+var imagen = document.getElementById("imagen");
+var butonclose = document.getElementById("close");
+boton.addEventListener("click", function(event) {
+    event.preventDefault();
+    imagen.style.display = "flex";
+});
+butonclose.addEventListener("click", function(event) {
+    event.preventDefault();
+    imagen.style.display = "none";
+});
+var boton2 = document.getElementById("deidara");
+var imagen2 = document.getElementById("imagen2");
+var butonclose2 = document.getElementById("close2");
+boton2.addEventListener("click", function(event) {
+    event.preventDefault();
+    imagen2.style.display = "flex";
+});
+butonclose2.addEventListener("click", function(event) {
+    event.preventDefault();
+    imagen2.style.display = "none";
+});
+var boton3 = document.getElementById("sasori");
+var imagen3 = document.getElementById("imagen3");
+var butonclose3 = document.getElementById("close3");
+boton3.addEventListener("click", function(event) {
+    event.preventDefault();
+    imagen3.style.display = "flex";
+});
+butonclose3.addEventListener("click", function(event) {
+    event.preventDefault();
+    imagen3.style.display = "none";
+});
+var boton4 = document.getElementById("orochimaru");
+var imagen4 = document.getElementById("imagen4");
+var butonclose4 = document.getElementById("close4");
+boton4.addEventListener("click", function(event) {
+    event.preventDefault();
+    imagen4.style.display = "flex";
+});
+butonclose4.addEventListener("click", function(event) {
+    event.preventDefault();
+    imagen4.style.display = "none";
+});
+var boton5 = document.getElementById("konan");
+var imagen5 = document.getElementById("imagen5");
+var butonclose5 = document.getElementById("close5");
+boton5.addEventListener("click", function(event) {
+    event.preventDefault();
+    imagen5.style.display = "flex";
+});
+butonclose5.addEventListener("click", function(event) {
+    event.preventDefault();
+    imagen5.style.display = "none";
+});
+var boton6 = document.getElementById("kisame");
+var imagen6 = document.getElementById("imagen6");
+var butonclose6 = document.getElementById("close6");
+boton6.addEventListener("click", function(event) {
+    event.preventDefault();
+    imagen6.style.display = "flex";
+});
+butonclose6.addEventListener("click", function(event) {
+    event.preventDefault();
+    imagen6.style.display = "none";
+});
+var boton7 = document.getElementById("pain");
+var imagen7 = document.getElementById("imagen7");
+var butonclose7 = document.getElementById("close7");
+boton7.addEventListener("click", function(event) {
+    event.preventDefault();
+    imagen7.style.display = "flex";
+});
+butonclose7.addEventListener("click", function(event) {
+    event.preventDefault();
+    imagen7.style.display = "none";
+});
+var boton8 = document.getElementById("obito");
+var imagen8 = document.getElementById("imagen8");
+var butonclose8 = document.getElementById("close8");
+boton8.addEventListener("click", function(event) {
+    event.preventDefault();
+    imagen8.style.display = "flex";
+});
+butonclose8.addEventListener("click", function(event) {
+    event.preventDefault();
+    imagen8.style.display = "none";
+});
+var boton9 = document.getElementById("kakuzu");
+var imagen9 = document.getElementById("imagen9");
+var butonclose9 = document.getElementById("close9");
+boton9.addEventListener("click", function(event) {
+    event.preventDefault();
+    imagen9.style.display = "flex";
+});
+butonclose9.addEventListener("click", function(event) {
+    event.preventDefault();
+    imagen9.style.display = "none";
+});
+var boton10 = document.getElementById("hidan");
+var imagen10 = document.getElementById("imagen10");
+var butonclose10 = document.getElementById("close10");
+boton10.addEventListener("click", function(event) {
+    event.preventDefault();
+    imagen10.style.display = "flex";
+});
+butonclose10.addEventListener("click", function(event) {
+    event.preventDefault();
+    imagen10.style.display = "none";
+});
+var boton11 = document.getElementById("zetsu");
+var imagen11 = document.getElementById("imagen11");
+var butonclose11 = document.getElementById("close11");
+boton11.addEventListener("click", function(event) {
+    event.preventDefault();
+    imagen11.style.display = "flex";
+});
+butonclose11.addEventListener("click", function(event) {
+    event.preventDefault();
+    imagen11.style.display = "none";
+});
+var boton12 = document.getElementById("sasuke");
+var imagen12 = document.getElementById("imagen12");
+var butonclose12 = document.getElementById("close12");
+boton12.addEventListener("click", function(event) {
+    event.preventDefault();
+    imagen12.style.display = "flex";
+});
+butonclose12.addEventListener("click", function(event) {
+    event.preventDefault();
+    imagen12.style.display = "none";
+});
+// var boton4 = document.getElementById("orochimaru");
+// var boton5 = document.getElementById("konan");
